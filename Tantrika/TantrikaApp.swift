cf811ed65730ce75eba525b00eb181d90ad36d26
Jianshuo Wang <jianshuo@hotmail.com>
2026-03-27 09:42:02 +0900

    Initial Commit

---
+++ Tantrika/TantrikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TantrikaApp.swift
+//  Tantrika
+//
+//  Created by Jianshuo Wang on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct TantrikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TantrikaTests/TantrikaTests.swift
@@ -0,0 +1,19 @@
+//
+//  TantrikaTests.swift
+//  TantrikaTests
+//
+//  Created by Jianshuo Wang on 2026/3/27.
+//
+
+import Testing
+@testable import Tantrika
+
+struct TantrikaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
